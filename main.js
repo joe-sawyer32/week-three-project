1e8e05f76a71c5a00d0305b2b3267bdc72db19bf
Joe <joe-sawyer32@users.noreply.github.com>
2017-06-13 02:03:58 -0500

    Overhauling event listeners

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ var display = document.querySelector(".display");
 
 var theEquation = "";
 var decimalString = "";
+var subCount = 0;
 
 // *********************
 
 function clearEquation() {
   theEquation = "";
+  decimalString = "";
   display.textContent = theEquation;
 }
 
@@ -17,14 +19,15 @@ function calculate() {
   display.textContent = theEquation;
 }
 
-function trimStrings(lastIndex) {
-  theEquation = theEquation.substr(0, lastIndex);
-  decimalString = decimalString.substr(0, lastIndex);
-}
-
-function addToStrings(char) {
+function addToEquation(char) {
   theEquation += char;
-  if (char == "x" || char) decimalString += char;
+  console.log(theEquation);
+  if (char == "+" || char == "x" || char == "/") {
+    decimalString = "";
+  } else {
+    decimalString += char;
+  }
+  console.log(decimalString);
   display.textContent = theEquation;
 }
 
@@ -34,73 +37,106 @@ function canAddDecimal() {
       return false;
     }
   }
+  return true;
 }
 
-function addToEquation(element) {
-  var button = element;
-  var buttonType = button.className;
-  var thisChar = button.textContent;
+function handleDecimal() {
+  if (canAddDecimal()) {
+    addToEquation(".");
+  }
+}
 
-  //   if (char == "-") {
-  //     if (theEquation.length == 0) {
-  //       theEquation += char;
-  //     } else if (lastChar != "." && lastChar != "-") {
-  //       theEquation += char;
-  //     } else if (buttonType == "operator" && getpreviousChar(lastIndex, char)) {
-  //       console.log(theEquation);
-  //       theEquation = theEquation.substr(0, lastIndex);
-  //       console.log(theEquation);
-  //       theEquation += char;
-  //     }
-
-  //     display.textContent = theEquation;
-  //   }
-
-  if (theEquation.length == 0) {
-    if (thisChar != "x" && thisChar != "/" && thisChar != "+") {
-      addToStrings(thisChar);
+function handleOperator(op) {
+  if (decimalString.length == 0) {
+    if (op.id == "sub") {
+      addToEquation(op.textContent);
     }
   } else {
-    var lastIndex = theEquation.length - 1;
-    var lastChar = theEquation[lastIndex];
-
+    var lastChar = theEquation[length - 1];
     switch (lastChar) {
-      case "+":
-      case "x":
+      case "*":
       case "/":
-        if (buttonType == "num" || thisChar == "." || thisChar == "-") {
-          addToStrings(thisChar);
-        } else {
-          trimStrings(lastIndex);
-          addToStrings(thisChar);
-        }
+      case "+":
         break;
-      case ".":
-        if (buttonType == "num") {
-          addToStrings(thisChar);
-        }
-        break;
-      case "-":
-        // if (buttonType == "num" || thisChar == "." || thisChar == "-") {
-        //   addToStrings(thisChar);
-        // } else {
-        //   trimStrings(lastIndex);
-        //   addToStrings(thisChar);
-        // }
-        break;
+
       default:
-        if (thisChar == ".") {
-          console.log("MADE IT THIS FAR");
-          if (canAddDecimal()) {
-            addToStrings(thisChar);
-          }
-        } else {
-          addToStrings(thisChar);
-        }
         break;
     }
   }
 }
+
+// function trimStrings(lastIndex) {
+//   theEquation = theEquation.substr(0, lastIndex);
+//   decimalString = decimalString.substr(0, lastIndex);
+// }
+
+// function addToStrings(char) {
+//   if (char == "-") {
+//     subCount++;
+//     console.log(subCount);
+//     if (subCount > 2) {
+//       return;
+//     }
+//   }
+//   theEquation += char;
+//   if (
+//     char == "x" ||
+//     char == "+" ||
+//     char == "/" ||
+//     (char == "-" && subCount == 1)
+//   ) {
+//     decimalString = "";
+//   } else {
+//     if (char != "-") {
+//       subCount = 0;
+//     }
+//     decimalString += char;
+//   }
+//   display.textContent = theEquation;
+// }
+
+// function addToEquation(element) {
+//   var button = element;
+//   var buttonType = button.className;
+//   var thisChar = button.textContent;
+
+//   if (theEquation.length == 0) {
+//     if (thisChar != "x" && thisChar != "/" && thisChar != "+") {
+//       addToStrings(thisChar);
+//     }
+//   } else {
+//     var lastIndex = theEquation.length - 1;
+//     var lastChar = theEquation[lastIndex];
+
+//     switch (lastChar) {
+//       case "+":
+//       case "x":
+//       case "/":
+//       case "-":
+//         if (buttonType == "num" || thisChar == "." || thisChar == "-") {
+//           addToStrings(thisChar);
+//         } else {
+//           trimStrings(lastIndex);
+//           addToStrings(thisChar);
+//         }
+//         break;
+//       case ".":
+//         if (buttonType == "num") {
+//           addToStrings(thisChar);
+//         }
+//         break;
+//       default:
+//         if (thisChar == ".") {
+//           if (canAddDecimal()) {
+//             addToStrings(thisChar);
+//           }
+//         } else {
+//           addToStrings(thisChar);
+//         }
+//         break;
+//     }
+//   }
+// }
 
 function setButton(element) {
   var button = element;
@@ -109,9 +145,15 @@ function setButton(element) {
     button.addEventListener("click", clearEquation);
   } else if (buttonType == "evaluator") {
     button.addEventListener("click", calculate);
-  } else if (buttonType != "display") {
+  } else if (buttonType == "num") {
     button.addEventListener("click", function() {
-      addToEquation(button);
+      addToEquation(button.textContent);
+    });
+  } else if (buttonType == "decimalPoint") {
+    button.addEventListener("click", handleDecimal);
+  } else if (buttonType == "operator") {
+    button.addEventListener("click", function() {
+      handleOperator(button);
     });
   }
 }
